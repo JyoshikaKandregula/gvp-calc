c35773ce3a1e2abaf489afedeb47e1bac17cef95
JyoshikaKandregula <jyoshika.kandregula@gmail.com>
2024-12-03 17:40:14 +0530

    updated file

--- src/main/java/com/gvp/demo/Adder.java
+++ src/main/java/com/gvp/demo/Adder.java
@@ -1,10 +1,8 @@
 package com.gvp.calc;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Adder{
-    public static void main(String[] args) {
-        SpringApplication.run(Adder.class, args);
+public class Adder {
+    public int add(int addend, int adder) {
+        int sum = addend + adder;
+        return sum;
     }
 }
